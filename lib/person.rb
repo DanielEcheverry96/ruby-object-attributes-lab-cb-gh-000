c312ff9d28331f4ac13a40e3767a5daafc692929
Daniel Echeverry Giraldo <dfecheverryg@unal.edu.co>
2018-01-26 06:21:08 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -4,7 +4,7 @@ class Person
     @name = name
   end
 
-  def name = (name)
+  def name= (name)
     @name = name
   end
 
@@ -12,7 +12,7 @@ class Person
     @job = job
   end
 
-  def breed = (job)
+  def breed= (job)
     @job = job
   end
 
